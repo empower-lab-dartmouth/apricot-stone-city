49fb87ea877ae5e2e1e4c767301956b0ce2e1278
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2022-06-20 05:45:00 -0400

    trying to add app.yaml

--- src/core/models/state/state.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.defaultUserInfo = void 0;
-exports.defaultUserInfo = {
-    lastTextMessage: '',
-    userId: ''
-};
--- src/core/util/make/unvalidated-types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*
- * Note: The underscore '_' here is used to disabiguate
- * these unvalidated types from
- * their validated counterparts elsewhere
- */
-exports.__esModule = true;
