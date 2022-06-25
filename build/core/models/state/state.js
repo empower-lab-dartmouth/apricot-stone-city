aa70d7837f02a5166d956f220bd105ef0086608c
Bansharee Ireen <bansharee.ireen.24@dartmouth.edu>
2022-06-25 06:07:13 -0400

    testing w removed build/ and node_modules/

--- build/core/models/state/state.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultUserInfo = void 0;
-exports.defaultUserInfo = {
-    lastTextMessage: '',
-    userId: '',
-};
-//# sourceMappingURL=state.js.map
--- build/core/util/make/unvalidated-types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/*
- * Note: The underscore '_' here is used to disabiguate
- * these unvalidated types from
- * their validated counterparts elsewhere
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=unvalidated-types.js.map
