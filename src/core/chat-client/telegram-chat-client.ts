f7086428b0fb64e36de42cbea01a433c83b14ed7
LearningLab <learninglab@lab2186m.tumo.lab>
2022-06-27 14:23:12 +0400

    second push to remove telegram-keyboard

--- src/core/chat-client/telegram-chat-client.ts
+++ src/core/chat-client/telegram-chat-client.ts
@@ -1,63 +1,61 @@
-import { ChatClientConstructor } from '../models/chat-client/chat-client';
-import ConvoManager from '../models/convo-engine/managers/convo-manager';
-import RenderInChat from '../models/chat-client/render-interface';
-import log from '../util/logging';
-import { Stores } from '../models/state/state';
-import { State } from '../../state/state-config';
+import { ChatClientConstructor } from '../models/chat-client/chat-client'
+import ConvoManager from '../models/convo-engine/managers/convo-manager'
+import RenderInChat from '../models/chat-client/render-interface'
+import log from '../util/logging'
+import { Stores } from '../models/state/state'
+import { State } from '../../state/state-config'
 
-const Telegraf = require('telegraf');
-const session = require('telegraf/session');
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
 
 type TelegrafContext = any
 type TelegrafBot = any
 
 function renderWithContext(ctx: TelegrafContext): RenderInChat {
-  return {
-    replyText: (text, buttons) => {
-      log.debug('reply in chat with the text message: ', text);
-      ctx.replyWithHTML(text);
-    },
-    replyImage: (src, buttons) => {
-      log.debug('reply in chat with the image: ', src);
-      ctx.replyWithPhoto(
-        { url: `${src}`, filename: 'photo.jpg' }
-      );
-    },
-  };
+    return {
+        replyText: (text, buttons) => {
+            log.debug('reply in chat with the text message: ', text)
+            ctx.replyWithHTML(text)
+        },
+        replyImage: (src, buttons) => {
+            log.debug('reply in chat with the image: ', src)
+            ctx.replyWithPhoto({ url: `${src}`, filename: 'photo.jpg' })
+        },
+    }
 }
 
-export const telegramClient: ChatClientConstructor = (apiKey) => {
-  const bot: TelegrafBot = new Telegraf(apiKey);
-  return {
-    runModule: (storytellerConfig, convoManagerConstructor) => {
-      bot.use(session());
+export const telegramClient: ChatClientConstructor = apiKey => {
+    const bot: TelegrafBot = new Telegraf(apiKey)
+    return {
+        runModule: (storytellerConfig, convoManagerConstructor) => {
+            bot.use(session())
 
-      const initStateStores: Stores = {
-        variables: storytellerConfig.initialState as State,
-        currentConvoSegmentPath:
+            const initStateStores: Stores = {
+                variables: storytellerConfig.initialState as State,
+                currentConvoSegmentPath:
                     storytellerConfig.startingConvoSegmentPath,
-      };
+            }
 
-      const convoManager: ConvoManager = convoManagerConstructor(
-        storytellerConfig.rootModule,
-        initStateStores,
-      );
+            const convoManager: ConvoManager = convoManagerConstructor(
+                storytellerConfig.rootModule,
+                initStateStores
+            )
 
-      bot.on('text', (ctx: any) => {
-        log.debug('received user input');
-        const renderFunctions: RenderInChat = renderWithContext(ctx);
-        convoManager.respondToUserInput(
-          ctx.from.id,
-          ctx.message.text,
-          renderFunctions,
-        );
-      });
-      bot.on('message', (ctx: { reply: (arg0: string) => any }) => {
-        log.debug('received user input as message other than text');
-        ctx.reply('Only text messages please');
-      });
-      log.debug('telegram client is configured and waiting for messages.');
-      bot.launch();
-    },
-  };
-};
+            bot.on('text', (ctx: any) => {
+                log.debug('received user input')
+                const renderFunctions: RenderInChat = renderWithContext(ctx)
+                convoManager.respondToUserInput(
+                    ctx.from.id,
+                    ctx.message.text,
+                    renderFunctions
+                )
+            })
+            bot.on('message', (ctx: { reply: (arg0: string) => any }) => {
+                log.debug('received user input as message other than text')
+                ctx.reply('Only text messages please')
+            })
+            log.debug('telegram client is configured and waiting for messages.')
+            bot.launch()
+        },
+    }
+}
